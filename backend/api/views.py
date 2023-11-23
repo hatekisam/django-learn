7d7bceb3d8702243f8159fa79e3b26c9267b6140
Mugisha Yves <mugishayves189000@gmail.com>
2023-11-23 10:32:40 +0200

    used rest framework

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,14 +1,16 @@
 import json
 from django.forms.models import  model_to_dict
-from django.http import JsonResponse,HttpResponse
+# from django.http import JsonResponse,HttpResponse
 from django.apps import  apps
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 Product = apps.get_model('products', 'Product')
 
+@api_view(["GET"])
 def api_home(request,*args,**kwargs):
     global json_data_str
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
         data = model_to_dict(model_data, fields=['id', 'title', 'price'])
-        json_data_str = json.dumps(data)
-    return HttpResponse(json_data_str,headers={"content-type": "application/json"})
+    return Response(data)
