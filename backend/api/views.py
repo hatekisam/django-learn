4c8a786922e022f2a67f2af80456d751a97d3cb3
Mugisha Yves <mugishayves189000@gmail.com>
2023-11-23 10:29:47 +0200

    seen the httpresponse

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,14 @@
 import json
-from django.http import JsonResponse
+from django.forms.models import  model_to_dict
+from django.http import JsonResponse,HttpResponse
 from django.apps import  apps
 Product = apps.get_model('products', 'Product')
 
 def api_home(request,*args,**kwargs):
+    global json_data_str
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
-    return JsonResponse(data)
+        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
+        json_data_str = json.dumps(data)
+    return HttpResponse(json_data_str,headers={"content-type": "application/json"})
