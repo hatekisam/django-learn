d71d3ee110b605e49fc7ef3d3faae0bf93e8d5c7
Mugisha Yves <mugishayves189000@gmail.com>
2023-11-23 10:22:46 +0200

    [fix] import of product model

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,14 +1,13 @@
 import json
-
-from django.http import  JsonResponse
+from django.http import JsonResponse
+from django.apps import  apps
+Product = apps.get_model('products', 'Product')
 
 def api_home(request,*args,**kwargs):
-    body = request.body
-    print(body)
-    data={}
-    try:
-        data = json.loads(body)
-    except:
-        pass
-    print(data)
+    model_data = Product.objects.all().order_by("?").first()
+    data = {}
+    if model_data:
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
     return JsonResponse(data)
